13fa7ea9c4490887f5561e6cbc10842ca92ee02c
Philippe Heymans Smith <pheymanss@gmail.com>
2020-10-23 16:30:53 -0600

    new functions: plot_columns report_columns
    these functions plot all columns of a table with ggplotly: boxplots for numerical variables and barplots for categoricals. plot_cols outputs a list of ggplotly objects and report_columns() a fully fledged html report.

---
+++ R/report_columns.R
@@ -0,0 +1,136 @@
+#' Plot all columns of a table
+#'
+#' Make boxplots for each numerical variable on a table, and barplots for each categorical variable.
+#'
+#' @param dt Table to be plotted.
+#' @param by_column Name of the colum to use as groups for all the other plots
+#'
+#' @return A list of plotly::ggplotly objects, one for each column of the table.
+#' @export
+#'
+#' @examples
+#' chronicle::report_columns(dt = iris, by_column = 'Species')
+plot_columns <- function(dt, by_column = NULL){
+  # numeric values
+  nums <- dt %>% purrr::keep(is.numeric) %>% colnames() %>% purrr::set_names(.)
+  if(length(nums) > 0){
+    num_plots <- nums %>% purrr::map(~make_boxplot(dt = dt,
+                                                   value = .x,
+                                                   groups = by_column,
+                                                   jitter = TRUE))
+  }
+
+  # categorical values
+  cats <- dt %>% purrr::discard(is.numeric) %>% colnames() %>% purrr::set_names(.)
+  if(length(cats) > 0){
+    cat_plots <- cats %>% purrr::map(~make_barplot(dt = dt,
+                                                   bars = .x,
+                                                   break_bars_by = by_column))
+  }
+  return(append(num_plots, cat_plots)[colnames(dt)])
+}
+
+
+#' HTML interactive report detailing each column on a table
+#'
+#' @param dt Table to be plotted.
+#' @param by_column Name of the colum to use as groups for all the other plots
+#' @param filename Name of the output file.
+#' @param author Author of the report.
+#' @param prettydoc Whether or not to use prettydoc formatting on the html report. Default is TRUE.
+#' @param prettydoc_theme Name of the theme used on prettydoc. Default is cayman.
+#' @param highlight Rmarkdown highlight theming. Default is github highlighting.
+#' @param number_sections Whether or not to number the sections and subsections fo the report.
+#' @param table_of_content Whether or not to include a table fo content at the beginning of the report.
+#' @param table_of_content_depth The depth of sections and subsections to be displayed on the table of content.
+#' @param fig_width Set the global figure width or the rmarkdown file.
+#' @param fig_height Set the global figure height or the rmarkdown file.
+#' @param directory The directory in which to render the .html report
+#' @param keep_rmd Whether or not to keep the .Rmd file. Default is false.
+#' @param render_html Whether or not to render the report as an interactive hmtl file.
+#' @param render_pdf Not working yet.
+#'
+#' @return An HTML file with a plot for each column on the given table: a boxplot for each numerical variable, and a barplot for each categorical variable.
+#' @export
+#'
+#' @examples chronicle::report_columns(dt = iris, by_column = 'Species', keep_rmd = T)
+report_columns <- function(dt,
+                            by_column = NULL,
+                            filename = NULL,
+                            author = 'chronicle user',
+                            prettydoc = TRUE,
+                            prettydoc_theme = 'leonids',
+                            highlight = 'github',
+                            number_sections = TRUE,
+                            table_of_content = TRUE,
+                            table_of_content_depth = 1,
+                            fig_width = 11,
+                            fig_height = 5,
+                            directory = getwd(),
+                            keep_rmd = FALSE,
+                            render_html = TRUE,
+                            render_pdf = FALSE){
+
+  # create report title
+  dt_name <- deparse(substitute(dt))
+  title <- paste(dt_name, 'Variable Analysis', if(!is.null(by_column)){paste('by', by_column)})
+
+  # create filename
+  if(is.null(filename)){
+    filename <- gsub('[^[:alnum:][:space:]]',
+                     '',
+                     dt_name) %>%
+      paste0('_column_analysis')
+  }
+
+  #create report header
+  column_report <- chronicle::new_report(title = title,
+                                               author = author,
+                                               prettydoc = prettydoc,
+                                               prettydoc_theme = prettydoc_theme,
+                                               highlight = highlight,
+                                               number_sections = number_sections,
+                                               table_of_content = table_of_content,
+                                               table_of_content_depth = table_of_content_depth)
+
+  # add sections for all numeric values
+  nums <- dt %>% purrr::keep(is.numeric) %>% colnames() %>% purrr::set_names(.)
+  if(length(nums) > 0){
+    num_plots <- nums %>% purrr::map(~add_boxplot(report = '',
+                                                  dt = dt_name,
+                                                  value = .x,
+                                                  groups = by_column,
+                                                  title_level = 1,
+                                                  jitter = TRUE))
+  }
+
+  # add sections for all categorical values
+  cats <- dt %>% purrr::discard(is.numeric) %>% colnames() %>% purrr::set_names(.)
+  if(length(cats) > 0){
+    cat_plots <- cats %>% purrr::map(~add_barplot(report = '',
+                                                  dt = dt_name,
+                                                  bars = .x,
+                                                  break_bars_by = by_column,
+                                                  title_level = 1))
+  }
+
+  # join both cats and nums
+  # arranged by column number but keeping `by_column` first
+  plot_sections <- append(num_plots,
+                          cat_plots)[c(by_column, colnames(dt)[-which(colnames(dt) == by_column)])] %>%
+    purrr::reduce(paste, sep = '\n')
+
+  # join header and plots
+  column_report %<>% paste(plot_sections, sep = '\n')
+
+  # render report
+  chronicle::render_report(report = column_report,
+                           filename = filename,
+                           directory = directory,
+                           keep_rmd = keep_rmd,
+                           render_html = render_html,
+                           render_pdf = render_pdf)
+
+}
+
+
