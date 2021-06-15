746e3beab14b0c9ec2cfba0f64c0fb5d5d358cca
pheymanss <pheymanss@gmail.com>
2021-06-14 23:49:59 -0600

    rewrite with glue

--- R/tables.R
+++ R/tables.R
@@ -5,7 +5,9 @@
 #' @param table_title title of the table. Default is no title.
 #' @param title_level Level of the section title of this plot (ie, number of # on Rmarkdown syntax.)
 #' @param html_table_type Either print a knitr::kable table or a DT htmlwidget.
-#' @param table_params Single character string with any additional parameters to be passed to either knitr::kable() or DT::datatable(), depending on html_table_type
+#' @param table_params A named list of additional parameters to be passed to either knitr::kable() or DT::datatable(), depending on html_table_type
+#' @param fig_width Width of the figures printed from this code.
+#' @param fig_height Height of the figures printed from this code.
 #'
 #' @return An R Markdown file as a character string, now containing a chunk for the specified table.
 #'
@@ -21,24 +23,32 @@ add_table <- function(report = '',
                       table_title = NULL,
                       title_level = 2,
                       html_table_type = c('DT', 'kable'),
-                      table_params = NULL){
+                      table_params = NULL,
+                      fig_width = NULL,
+                      fig_height = NULL
+                      ){
   html_table_type <- match.arg(arg = html_table_type, choices = c('DT', 'kable'))
 
-  report <- paste(report, # report object (Rmarkdown file text)
-                  paste(paste(rep('#', title_level), collapse = ''), table_title),
-                  paste('```{r, echo = FALSE, message = FALSE, warning = FALSE}', # r chunk header
-                        # create_table
-                        paste(paste(c(ifelse(test = html_table_type == 'DT',
-                                      yes = 'DT::datatable(',
-                                      no = 'knitr::kable('),
-                                      deparse(substitute(table)),
-                                      table_params[1],
-                                      ')'),
-                                    collapse = '')
-                          , collapse = '\n'),
-                        #finish
-                        '```', sep = '\n'),
-                  sep = '\n\n')
+  if(!is.null(table_title)){
+    table_title <- paste0('\n', chronicle::rmd_title_level(title_level), ' ', table_title)
+  }
+
+
+  table_call <- paste('```{r, echo=FALSE, message=FALSE, warning=FALSE}',
+                      # create_table
+                      paste(paste(c(ifelse(test = html_table_type == 'DT',
+                                           yes = 'DT::datatable(',
+                                           no = 'knitr::kable('),
+                                    deparse(substitute(table)),
+                                    table_params[1],
+                                    ')'),
+                                  collapse = '')
+                            , collapse = '\n'),
+                      '```', sep = '\n')
+
+  report <- glue::glue("{report}
+{ifelse(is.null(table_title), '', table_title)}
+{table_call}", .trim=TRUE)
+
   return(report)
 }
-
